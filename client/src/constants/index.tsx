00f069c208df9070520bc0404c9d367ef8cf8969
i8lex <cybeerid@gmail.com>
2023-09-23 20:41:53 +0300

    added run build

--- client/src/constants/index.tsx
+++ client/src/constants/index.tsx
@@ -1,4 +1,5 @@
-export const URL =
-  "https://hiring-assessment-server-7206a80f9bd6.herokuapp.com/api";
+// export const URL =
+// "https://hiring-assessment-server-7206a80f9bd6.herokuapp.com/api";
 
 // export const URL = "http://localhost:3001/api";
+export const URL = "https://hiringsystem.adaptable.app/api";
